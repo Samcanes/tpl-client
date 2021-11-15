26f0f17b3a07e717c855498ad626510e3b6be999
Irtesam Virani <irtesam.virani@gmail.com>
2021-11-15 14:38:52 +0530

    added sort

--- src/app/update-project/update-project.component.ts
+++ src/app/update-project/update-project.component.ts
@@ -10,30 +10,66 @@ import { MatButtonToggleModule } from '@angular/material/button-toggle';
 })
 export class UpdateProjectComponent implements OnInit {
   public projects = [];
+  public projectsData = [];
+
   p: number = 1;
   key: string = 'id';
   reverse: boolean = false;
+  deptDataArray = [];
+  loctDataArray = [];
 
   // public depselectionArray = [];
   public searchTearm: String = '';
+  public searchLoctTerm: String = '';
+  public searchDeptTerm: String = '';
 
   // statusRegistered: Boolean = false;
   constructor(private _projects: AuthProjectEventsService) {}
 
+  duplicateRemover(arr: any) {
+    var tmp: any = [];
+    // console.log(arr);
+    for (var i = 0; i < arr.length; i++) {
+      if (tmp.indexOf(arr[i]) == -1) {
+        // console.log(arr[i]);
+        tmp.push(arr[i]);
+      }
+    }
+    // console.log(tmp);
+    return tmp;
+  }
+
   ngOnInit() {
     this._projects.readAllProjects().subscribe(
       (res) => {
-        console.log(res.projectData);
+        // console.log(res.projectData);
         this.projects = res.projectData;
+        this.projectsData = res.projectData;
+
+        // console.log(this.projects);
+        // let temp: any =
+        this.projects.forEach((element, index) => {
+          // console.log(index);
+          this.deptDataArray[index] = element['department'];
+          this.loctDataArray[index] = element['location'];
+        });
+        console.log(this.loctDataArray, this.deptDataArray);
+
+        this.loctDataArray = this.duplicateRemover(this.loctDataArray);
+
+        this.deptDataArray = this.duplicateRemover(this.deptDataArray);
+
+        console.log(this.loctDataArray, this.deptDataArray);
       },
       (err) => console.log(err)
     );
   }
+
   Search() {
     if (this.searchTearm == '') {
       this.ngOnInit();
     } else {
-      this.projects = this.projects.filter((res) => {
+      this.projects = this.projectsData.filter((res) => {
         console.log(res['name']);
         let basicRes: any = res['name'];
         return basicRes
@@ -42,6 +78,41 @@ export class UpdateProjectComponent implements OnInit {
       });
     }
   }
+  dropdownDeptSelect() {
+    console.log(this.searchDeptTerm);
+    if (this.searchDeptTerm == '') {
+      this.ngOnInit();
+    } else {
+      console.log('I am here');
+      this.projects = this.projectsData.filter((res) => {
+        console.log(res);
+        console.log(res['department']);
+        let basicRes: any = res['department'];
+        console.log(this.searchDeptTerm.toLocaleLowerCase());
+        return basicRes
+          .toLocaleLowerCase()
+          .match(this.searchDeptTerm.toLocaleLowerCase());
+      });
+    }
+  }
+  dropdownLoctSelect() {
+    console.log(this.searchLoctTerm);
+    if (this.searchLoctTerm == '') {
+      this.ngOnInit();
+    } else {
+      console.log('I am here');
+      this.projects = this.projectsData.filter((res) => {
+        console.log(res);
+        console.log(res['location']);
+        let basicRes: any = res['location'];
+        console.log(this.searchLoctTerm.toLocaleLowerCase());
+        return basicRes
+          .toLocaleLowerCase()
+          .match(this.searchLoctTerm.toLocaleLowerCase());
+      });
+    }
+  }
+
   sort(key: any) {
     console.log('sort');
     this.key = key;
